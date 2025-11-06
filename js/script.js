34a530d6a15e81f96e4134475570fea6e6748bf8
atorres41 <atorres41@hawk.iit.edu>
2025-11-06 14:58:20 -0600

    alert_box_initialization

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+
+
+  document.getElementById("entrybutton").addEventListener("click", function() {
+    alert("Hello World!");
+    document.getElementById("textoutput").innerHTML = "New text!";
+  });
 
 
 
-
-
-
-
+}
 window.addEventListener('load', init);
