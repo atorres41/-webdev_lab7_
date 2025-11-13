fb9a152dfdd02941b217926427dab1fdebf2186e
atorres41 <atorres41@hawk.iit.edu>
2025-11-12 21:27:35 -0600

    complete_v1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 function init(){
 //add your javascrip between these two lines of code
 
+  const button = document.getElementById("entrybutton");
+  const input = document.getElementById("entryinput");
+  const output = document.getElementById("textoutput");
 
   document.getElementById("entrybutton").addEventListener("click", function() {
-    alert("Hello World!");
+    const message = input.value;
+    alert("Anthony Torres: " + message);
     document.getElementById("textoutput").innerHTML = "New text!";
+    output.innerText = message;
   });
 
 
